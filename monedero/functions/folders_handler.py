c4898c3b15efd3101388b6925780033907f97101
SebasRossi <sebastian.p.rossi@gmail.com>
2024-01-19 01:58:27 -0300

    completado

--- monedero/functions/folders_handler.py
+++ monedero/functions/folders_handler.py
@@ -3,38 +3,39 @@
 
 # TODO:
 #   comprobar si .env existe - DONE
-#   si no existe, crearlo
-#   si existe, ver si esta vacio
-#   si esta vacio, pasar el path que vino por CLI
-#   si no vino path, usar el default
-#   devolver path de carpetas
+#   si no existe, crearlo - DONE
+#   si existe, ver si esta vacio - DONE
+#   si esta vacio, pasar el path que vino por CLI - DONE
+#   si no vino path, usar el default - DONE
+#   devolver path de carpetas - DONE
 
 
-from os import path, getcwd
+from os import path
 from pathlib import Path
-#import os
 
 
 ENV_FILE = "monedero/_data_env/.env"  # para hacerlo configurable, pasar a clase todo esto
-DATA_SAVING_FOLDER_DEFAULT = f"{str(Path.home())}/Documents"
+DATA_SAVING_FOLDER_DEFAULT = f"{str(Path.home())}/Documents/"
 
 def environment(saving_path:str=None) -> str: # deberia ser un path
-
-    # https://www.earthdatascience.org/courses/intro-to-earth-data-science/python-code-fundamentals/work-with-files-directories-paths-in-python/set-working-directory-os-package/
+    """
+        lee si existe o crea si no el archivo con configuraciones, en este caso de la carpeta de 
+        guardado de archivos.
+    """
 
     if not path.isfile(ENV_FILE): 
-        create_env(ENV_FILE)
+        create_env(ENV_FILE) # innecesaria la funcion pero queda para despues
     
-    carpeta=read_data_from_env(ENV_FILE)["carpeta"]
+    carpeta=read_data_from_env(ENV_FILE)["carpeta"] # algo no me gusta de esto
 
     if carpeta == 'None':
         if saving_path == None:
             carpeta=DATA_SAVING_FOLDER_DEFAULT
         else :
             carpeta=saving_path
-            write_path_to_env(ENV_FILE)
+            write_path_to_env(ENV_FILE, carpeta)
     
-    return  carpeta #getcwd() # carpeta
+    return  carpeta
 
 
 def create_env(env):
@@ -48,7 +49,11 @@ def read_data_from_env(env):
     return dictionary
 
 
-def write_path_to_env(env):
-    #encontrar la linea carpeta y reemplazarla 
-    #con json es facil, hagamosla con txtg
-    pass
+def write_path_to_env(env, folder):
+    # esto esta feo, pero para el caso esta bien
+    with open(env,'r') as file: 
+        data = file.readlines() 
+    data[0] = f"carpeta:{folder}"
+    with open(env,'w') as file: 
+        file.writelines(data) 
+
